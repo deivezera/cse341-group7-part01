b8ce0c55020a4f553dcc51291941c52908a2442c
josephwilms7 <josephwilmsdev@gmail.com>
2025-02-06 18:09:53 -0700

    Display username when logging in

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ passport.deserializeUser((user,done) => {
   done(null, user);
 })
 
-app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out') });
+app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.username}` : 'Logged Out') });
 
 app.get('/github/callback', 
   passport.authenticate('github', { failureRedirect: '/api-docs', session: false }), 
